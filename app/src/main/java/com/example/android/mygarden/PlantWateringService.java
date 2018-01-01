e039a2792667563758183bf19dd2132c3cea6bf9
ichanafisah <ichanfs524@gmail.com>
2018-01-01 15:09:20 +0700

    Grid View

--- app/src/main/java/com/example/android/mygarden/PlantWateringService.java
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -38,10 +38,10 @@ public class PlantWateringService extends IntentService {
      */
     public static void startActionWaterPlant(Context context, long plantId) {
         Intent intent = new Intent(context, PlantWateringService.class);
-        intent.setAction(ACTION_WATER_PLANTS);
+
         intent.setAction(ACTION_WATER_PLANT);
         intent.putExtra(EXTRA_PLANT_ID, plantId);
-        intent.setAction(ACTION_WATER_PLANTS);
+
         context.startService(intent);
     }
 
@@ -119,6 +119,7 @@ public class PlantWateringService extends IntentService {
         }
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, PlantWidgetProvider.class));
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_grid_view);
         //Now update all widgets
         PlantWidgetProvider.updatePlantWidgets(this, appWidgetManager, imgRes, plantId, canWater, appWidgetIds);
     }
